e1a2e75f13f2318e72372a283b078b236439f92b
jusper1 <hafis@gmail.com>
2026-09-17 14:23:36 +0700

    tambahkan cors

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,7 @@ import (
 	"iot-backend/internal/repositories"
 	"iot-backend/internal/services"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -71,6 +72,27 @@ func main() {
 
 
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+	AllowOrigins: []string{
+		"http://localhost:5173",
+		"http://127.0.0.1:5173",
+	},
+	AllowMethods: []string{
+		"GET",
+		"POST",
+		"PUT",
+		"DELETE",
+		"OPTIONS",
+	},
+	AllowHeaders: []string{
+		"Origin",
+		"Content-Type",
+		"Accept",
+		"Authorization",
+	},
+	AllowCredentials: true,
+}))
 
 	// PUBLIC
 	auth := router.Group("/auth")
@@ -134,4 +156,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+
 }
